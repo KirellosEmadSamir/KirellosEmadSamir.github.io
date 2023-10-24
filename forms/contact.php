89d2b34729a314faec22c4ac9e5151b4855fd27c
Kirellos Emad Samir <72670934+KirellosEmadSamir@users.noreply.github.com>
2023-10-24 14:06:02 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,5 @@
 <?php
   $receiving_email_address = 'kirellos_emad@yahoo.com';
-
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
@@ -16,7 +15,7 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+  /*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
@@ -24,7 +23,7 @@
     'port' => '587'
   );
   
-
+*/
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
